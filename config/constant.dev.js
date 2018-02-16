e0ac70dab5e1beeab25071cf24605eadc2c4be53
rhinnyglory <rhinny.glory@credencys.com>
2018-02-16 19:42:02 +0530

    changes in video

--- config/constant.dev.js
+++ config/constant.dev.js
@@ -2,5 +2,5 @@ app.constant('constant', {
     appName: 'WebRTC',
     env: 'dev',
     pageUrl: 'https://dev.credencys.com/webrtc/front/#/',
-    apiUrl: 'https://localhost:3031/'
+    apiUrl: 'https://dev.credencys.com:3031/'
 });
